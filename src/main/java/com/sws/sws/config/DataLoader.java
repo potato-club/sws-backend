2d39cc0e9eda7be77727d54bdbd661c0508edfcc
seonghun <sdzx0719@naver.com>
2024-03-20 14:38:05 +0900

    add(service): tag enum타입들 db에 삽입

--- src/main/java/com/sws/sws/config/DataLoader.java
+++ src/main/java/com/sws/sws/config/DataLoader.java
@@ -1,30 +1,44 @@
-//package com.sws.sws.config;
-//
-//import com.sws.sws.entity.CategoryEntity;
-//import com.sws.sws.enums.CategoryName;
-//import com.sws.sws.repository.CategoryRepository;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//@RequiredArgsConstructor
-//public class DataLoader implements CommandLineRunner {
-//
-//    private final CategoryRepository categoryRepository;
-//
-//    @Override
-//    public void run(String... args) throws Exception {
+package com.sws.sws.config;
+
+import com.sws.sws.entity.CategoryEntity;
+import com.sws.sws.entity.TagEntity;
+import com.sws.sws.enums.CategoryName;
+import com.sws.sws.enums.TagName;
+import com.sws.sws.repository.CategoryRepository;
+import com.sws.sws.repository.TagRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DataLoader implements CommandLineRunner {
+
+    private final CategoryRepository categoryRepository;
+    private final TagRepository tagRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
 //        initializeCategories();
-//    }
-//
-//    // 카테고리 초기 enum타입들 데이터베이스에 삽입!
-//    private void initializeCategories() {
-//        for (CategoryName categoryName : CategoryName.values()) {
-//            CategoryEntity categoryEntity = CategoryEntity.builder()
-//                    .name(categoryName)
-//                    .build();
-//            categoryRepository.save(categoryEntity);
-//        }
-//    }
-//}
+        initializeTags();
+    }
+
+    // 카테고리 초기 enum타입들 데이터베이스에 삽입!
+    private void initializeCategories() {
+        for (CategoryName categoryName : CategoryName.values()) {
+            CategoryEntity categoryEntity = CategoryEntity.builder()
+                    .name(categoryName)
+                    .build();
+            categoryRepository.save(categoryEntity);
+        }
+    }
+
+        private void initializeTags() {
+        for (TagName tag : TagName.values()) {
+            TagEntity tagEntity = TagEntity.builder()
+                    .tag(tag)
+                    .build();
+            tagRepository.save(tagEntity);
+        }
+    }
+}
---
+++ src/main/java/com/sws/sws/repository/TagRepository.java
@@ -0,0 +1,8 @@
+package com.sws.sws.repository;
+
+import com.sws.sws.entity.TagEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TagRepository extends JpaRepository<TagEntity, Long> {
+
+}
