bdf577e97961403c87690c1744808ec25e955f30
soapcookie <sgua0720@gmail.com>
2024-04-10 18:00:37 +0900

    fix : repository

--- src/main/java/com/sws/sws/repository/CategoryRepository.java
+++ src/main/java/com/sws/sws/repository/CategoryRepository.java
@@ -3,9 +3,11 @@ package com.sws.sws.repository;
 import com.sws.sws.entity.CategoryEntity;
 import com.sws.sws.enums.CategoryName;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
 
     Optional<CategoryEntity> findByName(CategoryName categoryName);
