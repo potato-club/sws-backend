3556dd551f4521a97672c3401561d8c516d57f95
soapcookie <sgua0720@gmail.com>
2024-05-20 23:44:04 +0900

    fix : corsee

--- src/main/java/com/sws/sws/config/WebConfig.java
+++ src/main/java/com/sws/sws/config/WebConfig.java
@@ -4,8 +4,6 @@ package com.sws.sws.config;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -26,14 +24,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000",
+                .allowedOrigins(
+                        "http://localhost:3000",
                         "https://localhost:3000",
-                        "https:/shallwestudy.store",
+                        "https://shallwestudy.store",
                         "https://www.shallwestudy.store")
                 .exposedHeaders("authorization", "refreshToken", "Set-Cookie")
                 .allowedHeaders("*")
                 .allowedMethods("*")
                 .allowCredentials(true);
     }
-
 }
